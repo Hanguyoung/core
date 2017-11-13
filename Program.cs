0bc814ac5866a1d0613a54c3ce4f3d8ebc1170af
han <rndud926@gmail.com>
2017-11-13 23:53:35 +0900

    qwe
    qwe

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Nagasena.Proc;
+using Nagasena.Sax;
+using System;
+
+namespace HelloCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Transmogrifier encoder = new Transmogrifier();
+            //EXIDecoder decoder = new EXIDecoder();
+        }
+    }
+}
